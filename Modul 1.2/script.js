2e3be9bfa7a3dc95d45fe108a91cde3d9738c2cd
AnnaHelene01 <annahelene2001@gmail.com>
2022-01-30 14:35:17 +0100

    ert

---
+++ Modul 1.2/script.js
@@ -0,0 +1,53 @@
+console.log("Tasks Modul 1.2");
+console.log("Exercise 1");
+// a) Change the text of the H1 element to "Shiny, new title".
+//b) Change the background color of the (body of the) page from beige to lightsteelblue.
+//c) Change the style on the button to use:
+//background-color: darkred;
+//border: 1px solid white;
+//color: white;
+//padding: 12px 24px;
+//text-align: center;
+//text-decoration: none;
+//display: inline-block;
+//font-size: 1.5em;
+console.log("a)");
+console.log("b)");
+console.log("c)");
+
+
+
+console.log("Exercise 2");
+//a) Console log out the text content of the second paragraph on the page.
+//b) Change the style of the first paragraph on the page to use font-size at 1.5em and italic font-style.
+//c) Add the class makeMeGreen to the H1 element.
+//d) Add the class makeMeBlue to the last P element.
+//e) Add an alt attribute to the (only) image on the page with the value "Giorgio A. Tsoukalos is a nutter!".
+//f) Add a <footer> element at the bottom of the body, containing the text "@2021 Whatever".
+//g) Add a link around all the text in the third paragraph that goes to the address: https://no.wikipedia.org/wiki/Dill
+//h) Remove the subheading element (H2).
+
+
+
+console.log("Exercise 3");
+//Given the Array of objects:
+//Populate the ol (with id "myList") with list items, that is given the value from the 
+//lang property in each object from the Array. (In short: List the Array on the web page.)
+let programmingLanguages = [
+    { lang: "C", rank: 5 },
+    { lang: "Go", rank: 7 },
+    { lang: "JavaScript", rank: 2 },
+    { lang: "C++", rank: 6 },
+    { lang: "Swift", rank: 9 },
+    { lang: "Python", rank: 1 },
+    { lang: "PHP", rank: 10 },
+    { lang: "Java", rank: 3 },
+    { lang: "R", rank: 8 },
+    { lang: "C#", rank: 4 }
+];
+
+
+
+console.log("Exercise 4");
+//Add an eventListener to the button (with id "tBtn") and make a 
+//function to toggle the class makeMeRed on and off for the footer element.
